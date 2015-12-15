32757117e15f9044d2977c7fdb997c34febb1335
sarabrent <sarabrentmccoy@gmail.com>
2015-12-15 14:23:50 -0500

    Works. Ready for review.

--- ttt.js
+++ ttt.js
@@ -1,13 +1,8 @@
 
-var xWinCount = 0;
-var oWinCount = 0;
-var tieCount = 0;
 var playerX = 'X';
 var playerO = 'O';
 var player = 'X';
 var moves = 0;
-var xMoves = 0;
-var oMoves = 0;
 var turn = 0;
 
 
@@ -18,11 +13,11 @@ var board = [empty, empty, empty, empty, empty, empty, empty, empty, empty];
 //MESSAGES
 //this is how things get communicated to the players
 var msg = function(message) {
-    $('.message').text(message);
-  };
+    $('#message').text(message);
+};
 
 
-  var resetBoard = function resetBoard() {
+var resetBoard = function resetBoard() {
   $('#board div').each(function(){
     $(this).html('');
   });
@@ -30,8 +25,6 @@ var msg = function(message) {
       array[index] = '';
   });
   moves = 0;
-  xMoves = 0;
-  oMoves = 0;
   switchTurns();
 };
 
@@ -43,14 +36,12 @@ var switchTurns = function() {
       turn = 1;
       player = playerO;
       msg("O's turn!");
-      return xMoves++;
     } else if (turn === 1) {
       turn = 0;
       player = playerX;
       msg("X's turn!");
-      return oMoves++;
     }
-  };
+};
 
 // var checkTie = function(square, index, array) {
 //   if (board.every(!empty){
@@ -62,105 +53,114 @@ var switchTurns = function() {
 //   }
 
 
-  $('#board').on('click', function(event){
+$('#board').on('click', function(event){
     if (player === playerX) {
       var squareClickedOn = event.target;
       var squareClickedId = $(squareClickedOn).attr('id');
       board[squareClickedId] = playerX;
-      xMoves++;
       $(squareClickedOn).html(playerX);
-    }
-    if (player === playerO) {
+      switchTurns();
+      someoneWins();
+    } else if (player === playerO) {
       var squareClickedOn = event.target;
       var squareClickedId = $(squareClickedOn).attr('id');
       board[squareClickedId] = playerO;
-      oMoves++;
       $(squareClickedOn).html(playerO);
-    }
-    if (someoneWins() = false) {
-      checkTie();
       switchTurns();
+      someoneWins();
+      
+    };
+    if (moves < 8) {
+      moves++;
+    } else {
+      msg("TIE!")
     }
- });
+    console.log(moves);
+});
 
 
 //---------------
 // WINNING
-
- var totalMoves = function() {
-    moves = xMoves + oMoves;
-    return moves;
-  }
 
 // var notEmpty(element, index, array){
 //   return element != '';
 // }
 
 var someoneWins = function() {
-    for(var index = 0; index < 3; index++) {
-      board.eq(3*index).text() === board.eq(3*index + 1).text() && board.eq(3*index).text() === board.eq(3*index +2) && board.eq(3*index).text() === player && board.eq(3*index).text() !== '') {
+  if ((board[0] === board[1] && board[1] === board[2] && board[2] === playerX)
+      ||(board[3] === board[4] && board[4] === board[5] && board[5] === playerX)
+      ||(board[6] === board[7] && board[7] === board[8] && board[8] === playerX)
+      ||(board[0] === board[3] && board[3] === board[6] && board[6] === playerX)
+      ||(board[1] === board[4] && board[4] === board[7] && board[7] === playerX)
+      ||(board[2] === board[5] && board[5] === board[8] && board[8] === playerX)
+      ||(board[0] === board[4] && board[4] === board[8] && board[8] === playerX)
+      ||(board[2] === board[4] && board[4] === board[6] && board[6] === playerX)) {
+      var winner = playerX;
+      console.log("winner = X");
+      resetBoard();
+      msg("X WON!");
       return true;
-    };
-}
 
-var checkForWinner = function() {
-  if someoneWins() = true {
-    resetBoard();
-    return winner = player;
+  } else if ((board[0] === board[1] && board[1] === board[2] && board[2] === playerO)
+      ||(board[3] === board[4] && board[4] === board[5] && board[5] === playerO)
+      ||(board[6] === board[7] && board[7] === board[8] && board[8] === playerO)
+      ||(board[0] === board[3] && board[3] === board[6] && board[6] === playerO)
+      ||(board[1] === board[4] && board[4] === board[7] && board[7] === playerO)
+      ||(board[2] === board[5] && board[5] === board[8] && board[8] === playerO)
+      ||(board[0] === board[4] && board[4] === board[8] && board[8] === playerO)
+      ||(board[2] === board[4] && board[4] === board[6] && board[6] === playerO)) {
+      var winner = playerO;
+      console.log("winner = O");
+      resetBoard();
+      msg("O WON!");
+      return true;
+  // } else if (){
+  //     resetBoard();
+  //     msg("TIE!");
+  //     return true;
+  } else {
+    return false;
   };
-}
+    // for(var index = 0; index < 3; index++) {
+    //   board.eq(3*index).text() === board.eq(3*index + 1).text() && board.eq(3*index).text() === board.eq(3*index +2) && board.eq(3*index).text() === player && board.eq(3*index).text() !== '') {
+    //   return true;
+    // };
+};
+// var tieGame = function() {
+//   if (moves === 9 && someoneWins === false){
+//       resetBoard();
+//       msg("It's a tie!");
+//       console.log("TIE!")
+      
+//       return true;
+//   } else {
+//       return false;
+//   };
 
-var addWins = function() {
-  checkForWinner();
-  if (winner === playerX) {
-    return xWinCount++;
-  }
-  else if (winner === playerO) {
-    return oWinCount++;
-  }
-  else {
-    return null;
-  };
-}
+// };
+
+// var checkForWinner = function() {
+//   if someoneWins() {
+//     resetBoard();
+//     return winner = player;
+//   };
+// };
+
+// var addWins = function() {
+//   checkForWinner();
+//   if (winner === playerX) {
+//     return xWinCount++;
+//   }
+//   else if (winner === playerO) {
+//     return oWinCount++;
+//   }
+//   else {
+//     return null;
+//   };
+// }
 
 
-    // if (board[0] === board[1] && board[1] === board[2] && board[2] === playerX
-    //   ||board[3] === board[4] && board[4] === board[5] && board[5] === playerX
-    //   ||board[6] === board[7] && board[7] === board[8] && board[8] === playerX
-    //   ||board[0] === board[3] && board[3] === board[6] && board[6] === playerX
-    //   ||board[1] === board[4] && board[4] === board[7] && board[7] === playerX
-    //   ||board[2] === board[5] && board[5] === board[8] && board[8] === playerX
-    //   ||board[0] === board[4] && board[4] === board[8] && board[8] === playerX
-    //   ||board[2] === board[4] && board[4] === board[6] && board[6] === playerX) {
-    //   var winner = playerX;
-    //   msg(winner + ' won that one! Play again?');
-    //   resetBoard();
-    //   return winner;
-
-    // } else if (board[0] === board[1] && board[1] === board[2] && board[2] === playerO
-    //   ||board[3] === board[4] && board[4] === board[5] && board[5] === playerO
-    //   ||board[6] === board[7] && board[7] === board[8] && board[8] === playerO
-    //   ||board[0] === board[3] && board[3] === board[6] && board[6] === playerO
-    //   ||board[1] === board[4] && board[4] === board[7] && board[7] === playerO
-    //   ||board[2] === board[5] && board[5] === board[8] && board[8] === playerO
-    //   ||board[0] === board[4] && board[4] === board[8] && board[8] === playerO
-    //   ||board[2] === board[4] && board[4] === board[6] && board[6] === playerO) {
-    //   var winner = playerO;
-    //   msg(winner + ' won that one! Play again?');
-    //   resetBoard();
-    //   return winner;
-    // } else if (totalMoves === 9) {
-    //   msg("It's a tie!")
-    //   resetBoard();
-    //   return 'tie';
-    } else if (moves === 9) {
-      tieGame();
-    }
-      else {
-      return null;
-    }
-
-  }
+    
 
 //how wins get tallied
 // var countingWins = function() {
@@ -189,4 +189,4 @@ var addWins = function() {
 
 $(document).ready(function() {
   resetBoard();
-})
+});
